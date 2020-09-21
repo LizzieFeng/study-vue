8f375c1c3c072c45cf25302f2242d5c6f3ca58a2
fengruyi <fengruyi@gsafety.com>
2020-09-21 17:12:53 +0800

    测试兄弟组件的生命周期

---
+++ src/components/BChild.vue
@@ -0,0 +1,58 @@
+<template>
+  <div class="container">
+      <h3>我是BChild组件--{{msg}}</h3>
+      <div>
+      </div>
+  </div>
+</template>
+
+<script>
+export default {
+    name: 'BChild',
+    data () {
+        return {
+        msg: 'Welcome to BChild'
+        }
+    },
+    beforeCreate() {
+       console.log('我是 BChild beforeCreate,this.msg=',this.msg);
+    //    this.msg = 'beforeCreate';
+    },
+
+    created() {
+       console.log('我是 BChild created,this.msg=',this.msg);
+    //    this.msg = 'created';
+    },
+    beforeMount() {
+       console.log('我是 BChild beforeMount,this.msg=',this.msg);
+    //    this.msg = 'beforeMount';
+    },
+    mounted() {
+       console.log('我是 BChild mounted,this.msg=',this.msg);
+    //    this.msg = 'mounted';
+    },
+
+    beforeUpdate() {
+       console.log('我是 BChild beforeUpdate,this.msg=',this.msg);
+    //    this.msg = 'beforeUpdate';
+    },
+    updated() {
+       console.log('我是 BChild updated,this.msg=',this.msg);
+    //    this.msg = 'updated';
+    },
+    beforeDestroy() {
+        console.log('我是 BChild beforeDestroy,this.msg=',this.msg);
+    //    this.msg = 'beforeDestroy';
+    },
+    destroyed() {
+        console.log('我是 BChild destroyed,this.msg=',this.msg);
+    //    this.msg = 'destroyed';
+    },
+}
+</script>
+
+<style scoped>
+    .container{
+        background: yellow;
+    }
+</style>
--- src/views/lifecycle/LifeCycle.vue
+++ src/views/lifecycle/LifeCycle.vue
@@ -6,7 +6,7 @@
       <ul>
           <li>
               <h2>
-                  测试父子组件的生命周期关系（看控制台输出）
+                  1、测试父子组件的生命周期关系（看控制台输出）
               </h2>
               <div class="testGroupContainer">
                   <div>
@@ -18,25 +18,47 @@
                   </div>
               </div>
           </li>
+          <li>
+              <h2>
+                  2、测试兄弟组件的生命周期关系（看控制台输出）
+              </h2>
+              <div class="testGroupContainer">
+                  <div>
+                      <button @click.stop="changeBro('Aparent')">加载AParent组件</button>
+                      <button @click.stop="changeBro('Bparent')">加载BParent组件</button>
+                      <button @click.stop="changeBro('')">卸载组件</button>
+                  </div>
+                  <div>
+                      <component :is="broCompName"></component>
+                  </div>
+              </div>
+          </li>
       </ul>
   </div>
 </template>
 
 <script>
-import Aparent from '../lifecycle/children/AParent'
+import Aparent from '@/components/AParent';
+import Bparent from '@/components/BParent';
 export default {
     name: 'LifeCycle',
     components:{
-        Aparent
+        Aparent,
+        Bparent
     },
     data () {
         return {
-        AparentFlag: false
+            AparentFlag: false,
+            broCompName: '',
         }
     },
     methods:{
         addAparent(existFlag) {
             this.AparentFlag = !! existFlag;
+        },
+
+        changeBro(broCompName) {
+            this.broCompName = broCompName;
         }
     }
 }
